1ff93324cd649c1c358eddb32488b57afdc0baa4
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2025-01-13 14:01:22 +0900

    Update CNG_AES_Encrypt_original.c

--- CNG_AES_Encrypt_original.c
+++ CNG_AES_Encrypt_original.c
@@ -14,8 +14,8 @@ void Encrypt()
 
 	//--------------------------------------------------------------------------------------------------
 	BYTE Plain[] = { 'F', 'a', 'S', ' ', '2', '0', '2', '5', ' ',
-					 'F', 'a', 'S', ' ', '2', '0', '2', '5', ' ',
-					 'F', 'a', 'S', ' ', '2', '0', '2', '5', '\0' };
+			 'F', 'a', 'S', ' ', '2', '0', '2', '5', ' ',
+			 'F', 'a', 'S', ' ', '2', '0', '2', '5', '\0' };
 	BYTE* Cipher = NULL;	
 	printf("Plain text: %s\n", Plain);
 	//--------------------------------------------------------------------------------------------------
@@ -25,65 +25,65 @@ void Encrypt()
 	DWORD	IVLength		= 0;	
 	DWORD	BlockLength		= 0;	
 	DWORD	bufferSize		= 0;	
-	DWORD	CipherLength	= 0;
+	DWORD	CipherLength		= 0;
 	//--------------------------------------------------------------------------------------------------
 	// 1. Open Algorithm Provider 
 	BCRYPT_ALG_HANDLE algHandle = NULL;
 	status = BCryptOpenAlgorithmProvider(
 			 &algHandle,			// Address of Algorithm Handle
-			 BCRYPT_AES_ALGORITHM,  // Cryptographic Algorithm Name 
-			 NULL,					// Not Usze
-			 0);					// Flags 
+			 BCRYPT_AES_ALGORITHM, 		// Cryptographic Algorithm Name 
+			 NULL,				// Not Usze
+			 0);				// Flags 
 	if (!NT_SUCCESS(status)) { printf("1\n"); return; } 
 	//--------------------------------------------------------------------------------------------------
 	// 2. Generate AES256 Key Object
 	BCRYPT_KEY_HANDLE KEY_HANDLE	= NULL; 
 	
 	BYTE	AES256Key[32] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-							  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+				  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 	status = BCryptGenerateSymmetricKey(
 			 algHandle,			// Algorithm Handle
-			 &KEY_HANDLE,		// Symmetric Key Handle 
+			 &KEY_HANDLE,			// Symmetric Key Handle 
 			 NULL,				// Not use
-			 0,					// Not use 
+			 0,				// Not use 
 			 AES256Key,			// Key 
-			 sizeof(AES256Key), // Key size 
+			 sizeof(AES256Key), 		// Key size 
 			 0);				// Flags 
 	if (!NT_SUCCESS(status)) { printf("2\n"); return; }
 	//--------------------------------------------------------------------------------------------------
 	// 3.Setting Algorithm Properties - Operation Mode CBC 
 	status = BCryptSetProperty(
-			 KEY_HANDLE,					// Symmetric Key Handle 
+			 KEY_HANDLE,				// Symmetric Key Handle 
 			 BCRYPT_CHAINING_MODE,			// The name of the property to set 
-			 (PBYTE)BCRYPT_CHAIN_MODE_CBC,  // Address of property value 
-			 sizeof(BCRYPT_CHAIN_MODE_CBC), // Property value size 
-			 0);							// Flags 
+			 (PBYTE)BCRYPT_CHAIN_MODE_CBC,  	// Address of property value 
+			 sizeof(BCRYPT_CHAIN_MODE_CBC), 	// Property value size 
+			 0);					// Flags 
 	if (!NT_SUCCESS(status)) { printf("3\n"); return; }
 	//--------------------------------------------------------------------------------------------------
 	// 4. Getting Algorithm Properties - IVLength = BlockLength
 	status = BCryptGetProperty(
 			 KEY_HANDLE,			// Symmetric Key Handle 
-			 BCRYPT_BLOCK_LENGTH,	// The name of the property to obtain
+			 BCRYPT_BLOCK_LENGTH,		// The name of the property to obtain
 			 (PBYTE)&IVLength,		// Address of buffer that receives the value
 			 sizeof(DWORD),			// Size of buffer that receives the value 
 			 &bufferSize,			// number of bytes that were copied to the buffer 
-			 0);					// Flags 
+			 0);				// Flags 
 	if (!NT_SUCCESS(status)) { printf("4\n"); return; }
 	BlockLength = IVLength;
 	//--------------------------------------------------------------------------------------------------
 	// 5. Operate AES256/CBC/PKCS7Padding - Calculate Ciphertext Length 
 	status = BCryptEncrypt(
 			 KEY_HANDLE,			// Symmetric Key Handle 
-			 Plain,					// Plaintext
+			 Plain,				// Plaintext
 			 sizeof(Plain),			// Size of Plaintext
-			 NULL,					// Not use  
-			 iv,					// IV
-			 IVLength,				// Size of IV
-			 NULL,					// Set NULL to calculate Ciphertext Length
-			 0,						// 0
+			 NULL,				// Not use  
+			 iv,				// IV
+			 IVLength,			// Size of IV
+			 NULL,				// Set NULL to calculate Ciphertext Length
+			 0,				// 0
 			 &CipherLength,			// Receives the size of Ciphertext
-			 BCRYPT_BLOCK_PADDING); // Flags for Padding 
+			 BCRYPT_BLOCK_PADDING); 	// Flags for Padding 
 	if (!NT_SUCCESS(status)) { printf("5\n"); return; }
 	//--------------------------------------------------------------------------------------------------
 	// 6. Operate AES256/CBC/PKCS7Padding - Encrypt Plain text 
@@ -91,15 +91,15 @@ void Encrypt()
 	if (Cipher == NULL) return; 
 	status = BCryptEncrypt(
 			 KEY_HANDLE,			// Symmetric Key Handle 
-			 Plain,					// Plaintext
+			 Plain,				// Plaintext
 			 sizeof(Plain),			// Size of Plaintext
-			 NULL,					// Not use  
-			 iv,					// IV
-			 IVLength,				// Size of IV
-			 Cipher,				// Ciphertext
+			 NULL,				// Not use  
+			 iv,				// IV
+			 IVLength,			// Size of IV
+			 Cipher,			// Ciphertext
 			 CipherLength,			// Size of Ciphertext
 			 &bufferSize,			// number of bytes that were copied to the buffer 
-			 BCRYPT_BLOCK_PADDING);
+			 BCRYPT_BLOCK_PADDING);		// Flags
 	if (!NT_SUCCESS(status)) { printf("6\n"); return; }
 	// --------------------------------------------------------------------------------------------------
 	// 7. Print Cipher text
