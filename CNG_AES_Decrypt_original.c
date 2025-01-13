e0aa459e2ef8f18eac908f63cbc901fbca93578c
Yong Jin Lee <yjtarzan@kookmin.ac.kr>
2025-01-13 13:59:42 +0900

    Update CNG_AES_Decrypt_original.c

--- CNG_AES_Decrypt_original.c
+++ CNG_AES_Decrypt_original.c
@@ -46,47 +46,47 @@ void Decrypt()
 				  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
 	status = BCryptGenerateSymmetricKey(
-			 algHandle,			// Algorithm Handle
-			 &KEY_HANDLE,		// Symmetric Key Handle 
-			 NULL,				// Not use
-			 0,					// Not use 
-			 AES256Key,			// Key 
-			 sizeof(AES256Key), // Key size 
-			 0);				// Flags 
+			 		   algHandle,			// Algorithm Handle
+			 		   &KEY_HANDLE,			// Symmetric Key Handle 
+			 		   NULL,			// Not use
+			 		   0,	 			// Not use 
+			 		   AES256Key,			// Key 
+			 		   sizeof(AES256Key), 		// Key size 
+			 		   0);				// Flags 
 	if (!NT_SUCCESS(status)) { printf("2\n"); return; }
 	//--------------------------------------------------------------------------------------------------
 	// 3.Setting Algorithm Properties - Operation Mode CBC 
 	status = BCryptSetProperty(
-			 KEY_HANDLE,					// Symmetric Key Handle 
-			 BCRYPT_CHAINING_MODE,			// The name of the property to set 
-			 (PBYTE)BCRYPT_CHAIN_MODE_CBC,  // Address of property value 
-			 sizeof(BCRYPT_CHAIN_MODE_CBC), // Property value size 
-			 0);							// Flags 
+			 	   KEY_HANDLE,					// Symmetric Key Handle 
+			 	   BCRYPT_CHAINING_MODE,			// The name of the property to set 
+			 	   (PBYTE)BCRYPT_CHAIN_MODE_CBC,  		// Address of property value 
+			 	   sizeof(BCRYPT_CHAIN_MODE_CBC), 		// Property value size 
+			 	   0);						// Flags 
 	if (!NT_SUCCESS(status)) { printf("3\n"); return; }
 	//--------------------------------------------------------------------------------------------------
 	// 4. Getting Algorithm Properties - IVLength = BlockLength
 	status = BCryptGetProperty(
-			 KEY_HANDLE,			// Symmetric Key Handle 
-			 BCRYPT_BLOCK_LENGTH,	// The name of the property to obtain
-			 (PBYTE)&IVLength,		// Address of buffer that receives the value
-			 sizeof(DWORD),			// Size of buffer that receives the value 
-			 &bufferSize,			// number of bytes that were copied to the buffer 
-			 0);					// Flags 
+			 	  KEY_HANDLE,			// Symmetric Key Handle 
+			 	  BCRYPT_BLOCK_LENGTH,		// The name of the property to obtain
+			 	  (PBYTE)&IVLength,		// Address of buffer that receives the value
+			 	  sizeof(DWORD),		// Size of buffer that receives the value 
+			 	  &bufferSize,			// number of bytes that were copied to the buffer 
+			 	  0);				// Flags 
 	if (!NT_SUCCESS(status)) { printf("4\n"); return; }
 	BlockLength = IVLength;
 	//--------------------------------------------------------------------------------------------------
 	// 5. Operate AES256/CBC/PKCS7Padding - Calculate Plaintext Length 
 	status = BCryptDecrypt(
 			 KEY_HANDLE,			// Symmetric Key Handle 
-			 Cipher,				// Ciphertext
+			 Cipher,			// Ciphertext
 			 sizeof(Cipher),		// Size of Ciphertext
-			 NULL,					// Not use  
-			 iv,					// IV
-			 IVLength,				// Size of IV
-			 NULL,					// Set NULL to calculate Plaintext Length
-			 0,						// 0
+			 NULL,				// Not use  
+			 iv,				// IV
+			 IVLength,			// Size of IV
+			 NULL,				// Set NULL to calculate Plaintext Length
+			 0,				// 0
 			 &PlainLength,			// Receives the size of Plaintext
-			 BCRYPT_BLOCK_PADDING); // Flags for Padding 
+			 BCRYPT_BLOCK_PADDING); 	// Flags for Padding 
 	if (!NT_SUCCESS(status)) { printf("5\n"); return; }
 	//--------------------------------------------------------------------------------------------------
 	// 6. Operate AES256/CBC/PKCS7Padding - Decrypt Ciphertext 
@@ -94,15 +94,15 @@ void Decrypt()
 	if (Plain == NULL) return;
 	status = BCryptDecrypt(
 			 KEY_HANDLE,			// Symmetric Key Handle 
-			 Cipher,				// Ciphertext
+			 Cipher,			// Ciphertext
 			 sizeof(Cipher),		// Size of Ciphertext
-			 NULL,					// Not use  
-			 iv,					// IV
-			 IVLength,				// Size of IV
-			 Plain,					// Plaintext
+			 NULL,				// Not use  
+			 iv,				// IV
+			 IVLength,			// Size of IV
+			 Plain,				// Plaintext
 			 PlainLength,			// Size of Plaintext
 			 &bufferSize,			// number of bytes that were copied to the buffer 
-			 BCRYPT_BLOCK_PADDING);
+			 BCRYPT_BLOCK_PADDING);		// Flags
 	if (!NT_SUCCESS(status)) { printf("6\n"); return; }
 	// --------------------------------------------------------------------------------------------------
 	// 7. Print Plain text
